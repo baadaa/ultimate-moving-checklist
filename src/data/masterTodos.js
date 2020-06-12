66fc9e003d81c3abb1c14360dc90933290cc4a92
Bumhan Yu <bumhan.yu@gmail.com>
2020-06-12 15:13:45 -0400

    add all todo items per master sheet

--- src/data/masterTodos.js
+++ src/data/masterTodos.js
@@ -70,6 +70,668 @@ const todos = [
     category: "",
     tags: [],
     content: "Confirm that the moving company you hired is licensed and insured"
+  },
+  {
+    done: false,
+    week: -6,
+    category: "",
+    tags: [],
+    content: "Make time for friends and relatives before moving day"
+  },
+  {
+    done: false,
+    week: -6,
+    category: "",
+    tags: [],
+    content:
+      "Collect free boxes from restaurants, liquor stores, and grocery stores"
+  },
+  {
+    done: false,
+    week: -6,
+    category: "",
+    tags: [],
+    content: "Research packing hacks"
+  },
+  {
+    done: false,
+    week: -6,
+    category: "",
+    tags: [],
+    content: "Label moving boxes with different colors for each room"
+  },
+  {
+    done: false,
+    week: -6,
+    category: "",
+    tags: [],
+    content: "Create an inventory sheet of all your valuables"
+  },
+  {
+    done: false,
+    week: -6,
+    category: "",
+    tags: [],
+    content: "Mark moving boxes that are fragile"
+  },
+  {
+    done: false,
+    week: -6,
+    category: "",
+    tags: [],
+    content: "Take photos of all electronics before unplugging them"
+  },
+  {
+    done: false,
+    week: -6,
+    category: "",
+    tags: [],
+    content: "Put all hardware in labeled baggies"
+  },
+  {
+    done: false,
+    week: -6,
+    category: "",
+    tags: [],
+    content: "Gather socks, T-shirts, towels, and linens to pad fragile items"
+  },
+  {
+    done: false,
+    week: -6,
+    category: "",
+    tags: [],
+    content:
+      "If moving into an apartment, find out about any moving day requirements"
+  },
+  {
+    done: false,
+    week: -6,
+    category: "",
+    tags: [],
+    content: "Address minor home repairs before moving out"
+  },
+  {
+    done: false,
+    week: -6,
+    category: "",
+    tags: [],
+    content: "If moving across country, have your vehicles services"
+  },
+  {
+    done: false,
+    week: -6,
+    category: "",
+    tags: [],
+    content: "If shipping your car, make sure to empty it"
+  },
+  {
+    done: false,
+    week: -6,
+    category: "",
+    tags: [],
+    content:
+      "Measure doorways, stairways, and elevators to make sure furniture will fit"
+  },
+  {
+    done: false,
+    week: -6,
+    category: "",
+    tags: [],
+    content: "Research your new community for things to do"
+  },
+  {
+    done: false,
+    week: -6,
+    category: "",
+    tags: [],
+    content: "Pack a little every day"
+  },
+  {
+    done: false,
+    week: -4,
+    category: "",
+    tags: [],
+    content:
+      "Purchase moving insurance to cover all your belongings during the move"
+  },
+  {
+    done: false,
+    week: -4,
+    category: "",
+    tags: [],
+    content:
+      "Confirm parking options for moving truck and check if you need a permit"
+  },
+  {
+    done: false,
+    week: -4,
+    category: "",
+    tags: [],
+    content: "Donate items that can't be packed or sold"
+  },
+  {
+    done: false,
+    week: -4,
+    category: "",
+    tags: [],
+    content: "Gather all financial and legal records in one place"
+  },
+  {
+    done: false,
+    week: -4,
+    category: "",
+    tags: [],
+    content: "Gather birth certificates and passports for everyone"
+  },
+  {
+    done: false,
+    week: -4,
+    category: "",
+    tags: [],
+    content: "Find a new landscaper and snow removal service"
+  },
+  {
+    done: false,
+    week: -4,
+    category: "",
+    tags: [],
+    content: "Return books to library"
+  },
+  {
+    done: false,
+    week: -4,
+    category: "",
+    tags: [],
+    content: "Return movie and/or video game rentals"
+  },
+  {
+    done: false,
+    week: -4,
+    category: "",
+    tags: [],
+    content: "Pick up clothes from the dry cleaner"
+  },
+  {
+    done: false,
+    week: -4,
+    category: "",
+    tags: [],
+    content: "Update your voter registration"
+  },
+  {
+    done: false,
+    week: -4,
+    category: "",
+    tags: [],
+    content: "Compile medical, dental, and optical records for everyone"
+  },
+  {
+    done: false,
+    week: -4,
+    category: "",
+    tags: [],
+    content: "If moving out of town, find a new doctor, dentist, and vet"
+  },
+  {
+    done: false,
+    week: -4,
+    category: "",
+    tags: [],
+    content: "If moving long distance, plan your routes and book hotels"
+  },
+  {
+    done: false,
+    week: -4,
+    category: "",
+    tags: [],
+    content: "Shop around for cable, internet, and phone bundles"
+  },
+  {
+    done: false,
+    week: -4,
+    category: "",
+    tags: [],
+    content: "Make sure pets have updated ID tags for their collars"
+  },
+  {
+    done: false,
+    week: -4,
+    category: "",
+    tags: [],
+    content:
+      "Request copies of vet records, and optain any necessary pet medication"
+  },
+  {
+    done: false,
+    week: -4,
+    category: "",
+    tags: [],
+    content:
+      "If moving out of the country, make sure pets have all necessary vaccinations"
+  },
+  {
+    done: false,
+    week: -4,
+    category: "",
+    tags: [],
+    content: "Make a moving day playlist to enjoy while moving and unpacking"
+  },
+  {
+    done: false,
+    week: -2,
+    category: "",
+    tags: [],
+    content: "Recycle or dispose of corrosives, flammables, and poisonous items"
+  },
+  {
+    done: false,
+    week: -2,
+    category: "",
+    tags: [],
+    content:
+      "Prepare two-weeks worth of meals, and use everything in the freezer"
+  },
+  {
+    done: false,
+    week: -2,
+    category: "",
+    tags: [],
+    content: "Return borrowed items from friends and family"
+  },
+  {
+    done: false,
+    week: -2,
+    category: "",
+    tags: [],
+    content: "Back up your computer"
+  },
+  {
+    done: false,
+    week: -2,
+    category: "",
+    tags: [],
+    content:
+      "Set up trash removal and recycling for your new home, and cancel your current service"
+  },
+  {
+    done: false,
+    week: -2,
+    category: "",
+    tags: [],
+    content: "Remove light bulbs from all lamps you plan to move"
+  },
+  {
+    done: false,
+    week: -2,
+    category: "",
+    tags: [],
+    content: "Clean out safe deposit box and gym lockers"
+  },
+  {
+    done: false,
+    week: -2,
+    category: "",
+    tags: [],
+    content: "Fill all your prescriptions"
+  },
+  {
+    done: false,
+    week: -2,
+    category: "",
+    tags: [],
+    content:
+      "Prepare a list of emergency service technicians and preferred providers"
+  },
+  {
+    done: false,
+    week: -2,
+    category: "",
+    tags: [],
+    content:
+      "For moving day, line up a babysitter or make arrangements for a playdate"
+  },
+  {
+    done: false,
+    week: -2,
+    category: "",
+    tags: [],
+    content: "Find somewhere safe for pets to go during the move"
+  },
+  {
+    done: false,
+    week: -2,
+    category: "",
+    tags: [],
+    content: "Send out moving announcements"
+  },
+  {
+    done: false,
+    week: -2,
+    category: "",
+    tags: [],
+    content:
+      "Use Google Maps to plan your new route to work by car or public transportation"
+  },
+  {
+    done: false,
+    week: -2,
+    category: "",
+    tags: [],
+    content: "Clean outdoor furniture before it's moved"
+  },
+  {
+    done: false,
+    week: -2,
+    category: "",
+    tags: [],
+    content: "Address change: all financial institutions, and employer"
+  },
+  {
+    done: false,
+    week: -2,
+    category: "",
+    tags: [],
+    content: "Address change: USPS for mail forwarding"
+  },
+  {
+    done: false,
+    week: -2,
+    category: "",
+    tags: [],
+    content: "Address change: all government offices"
+  },
+  {
+    done: false,
+    week: -2,
+    category: "",
+    tags: [],
+    content: "Address change: newspaper and magazine subscriptions"
+  },
+  {
+    done: false,
+    week: -2,
+    category: "",
+    tags: [],
+    content: "Address change: DMV for driver's license"
+  },
+  {
+    done: false,
+    week: -2,
+    category: "",
+    tags: [],
+    content: "Address change: Amazon and monthly subscription boxes"
+  },
+  {
+    done: false,
+    week: -2,
+    category: "",
+    tags: [],
+    content: "Address change: auto insurance and car registration"
+  },
+  {
+    done: false,
+    week: -2,
+    category: "",
+    tags: [],
+    content: "Transfer accounts: homewoners or renters insurance"
+  },
+  {
+    done: false,
+    week: -2,
+    category: "",
+    tags: [],
+    content: "Transfer accounts: gym membership—or cancel it"
+  },
+  {
+    done: false,
+    week: -2,
+    category: "",
+    tags: [],
+    content:
+      "Transfer accounts: utilities—including water, electricity, and gas"
+  },
+  {
+    done: false,
+    week: -2,
+    category: "",
+    tags: [],
+    content: "Transfer accounts: prescriptions to a new pharmacy"
+  },
+  {
+    done: false,
+    week: -1,
+    category: "",
+    tags: [],
+    content: "Donate unopened food"
+  },
+  {
+    done: false,
+    week: -1,
+    category: "",
+    tags: [],
+    content:
+      "Make sure scheduled deliveries have been canceled and/or redirected"
+  },
+  {
+    done: false,
+    week: -1,
+    category: "",
+    tags: [],
+    content: "Clean your current home for next residents"
+  },
+  {
+    done: false,
+    week: -1,
+    category: "",
+    tags: [],
+    content: "Unplug the fridge and freezer to defrost the night before"
+  },
+  {
+    done: false,
+    week: -1,
+    category: "",
+    tags: [],
+    content: "Fill nail holes with a bar of soap"
+  },
+  {
+    done: false,
+    week: -1,
+    category: "",
+    tags: [],
+    content: "Drain water hoses to washing machine and ice maker"
+  },
+  {
+    done: false,
+    week: -1,
+    category: "",
+    tags: [],
+    content:
+      "Empty oil and gas from grills, heaters, lawn mowers, and snow blowers"
+  },
+  {
+    done: false,
+    week: -1,
+    category: "",
+    tags: [],
+    content: "Leave a note or small gift for new residents"
+  },
+  {
+    done: false,
+    week: -1,
+    category: "",
+    tags: [],
+    content:
+      "Conduct a final walkthrough for anything hidden onshelves or in closets"
+  },
+  {
+    done: false,
+    week: -1,
+    category: "",
+    tags: [],
+    content: "Watch the weather channel and be prepared for rain or snow"
+  },
+  {
+    done: false,
+    week: -1,
+    category: "",
+    tags: [],
+    content:
+      "Pack an essential box with everything you'll need for your first 24 hours in your new home"
+  },
+  {
+    done: false,
+    week: -1,
+    category: "",
+    tags: [],
+    content: "Make a worst-case scenario plan in case your movers run late"
+  },
+  {
+    done: false,
+    week: -1,
+    category: "",
+    tags: [],
+    content:
+      "Take photographs of your empty place to prove it's in move-out condition"
+  },
+  {
+    done: false,
+    week: -1,
+    category: "",
+    tags: [],
+    content: "Thoroughly clean your new place"
+  },
+  {
+    done: false,
+    week: 0,
+    category: "",
+    tags: [],
+    content: "Set an alarm and get up early"
+  },
+  {
+    done: false,
+    week: 0,
+    category: "",
+    tags: [],
+    content: "Protect your floor and carpets during move"
+  },
+  {
+    done: false,
+    week: 0,
+    category: "",
+    tags: [],
+    content: "Tip your movers"
+  },
+  {
+    done: false,
+    week: 0,
+    category: "",
+    tags: [],
+    content: "Make your bed and unpack your shower curtain and toiletries"
+  },
+  {
+    done: false,
+    week: 0,
+    category: "",
+    tags: [],
+    content: "Get to know your neighbors"
+  },
+  {
+    done: false,
+    week: 0,
+    category: "",
+    tags: [],
+    content: "Go grocery shopping"
+  },
+  {
+    done: false,
+    week: 0,
+    category: "",
+    tags: [],
+    content: "Cover your windows with sheets for privacy for your first night"
+  },
+  {
+    done: false,
+    week: 0,
+    category: "",
+    tags: [],
+    content: "Thank your real estate agent or broker"
+  },
+  {
+    done: false,
+    week: 1,
+    category: "",
+    tags: [],
+    content: "Leave a review for your moving company"
+  },
+  {
+    done: false,
+    week: 1,
+    category: "",
+    tags: [],
+    content: "Post leftover moving boxes or unwanted furniture on Craigslist"
+  },
+  {
+    done: false,
+    week: 1,
+    category: "",
+    tags: [],
+    content: "Clean the carpets in your new home"
+  },
+  {
+    done: false,
+    week: 1,
+    category: "",
+    tags: [],
+    content: "Swwp your chimney before using it"
+  },
+  {
+    done: false,
+    week: 1,
+    category: "",
+    tags: [],
+    content: "Make sure your home's address is visible from the street"
+  },
+  {
+    done: false,
+    week: 1,
+    category: "",
+    tags: [],
+    content: "Install a new home security system"
+  },
+  {
+    done: false,
+    week: 1,
+    category: "",
+    tags: [],
+    content: "Explore your new neighborhood"
+  },
+  {
+    done: false,
+    week: 1,
+    category: "",
+    tags: [],
+    content: "Send thank you notes to friends and family who helped you move"
+  },
+  {
+    done: false,
+    week: 1,
+    category: "",
+    tags: [],
+    content: "Unpack all your belongings within two weeks of moving in"
+  },
+  {
+    done: false,
+    week: 1,
+    category: "",
+    tags: [],
+    content: "Make spare copies of new keys"
+  },
+  {
+    done: false,
+    week: 1,
+    category: "",
+    tags: [],
+    content: "Celebrate!!!"
   }
 ];
 
